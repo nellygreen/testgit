6df6068ed640928e328b8758c50756e8611982d7
Nelly Green <nellygreen.cg@gmail.com>
2021-03-26 06:10:11 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,9 @@
+
+<section>
+    <div style="text-align:center;margin:200px;">
+        <h1>TEST GITHUB & XAMPP CONNECTION</h1>
+        <p style="text-transform:uppercase;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id nisi porro distinctio minima ipsum hic nihil dignissimos omnis incidunt iste?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, rem. Lorem ipsum dolor sit amet.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum iure aliquam maxime ullam accusantium nobis odit, ad, itaque architecto esse a accusamus asperiores sequi in tenetur, neque fugiat ab nesciunt! Reiciendis, doloribus qui at eum, laboriosam quod veritatis excepturi dolorum minima quasi nobis nisi. Accusantium magni sint, consequuntur cumque praesentium error, veniam distinctio. Officia nisi autem distinctio a eius aliquam sint placeat laborum maiores molestias, ipsam iure voluptate eum nihil, pariatur natus aliquid accusamus ad perferendis officiis rem unde expedita iusto quis? Quod iste placeat ipsam itaque veniam adipisci vero voluptatum cum perspiciatis autem sunt, veritatis, id magnam. Dolorem voluptatibus necessitatibus asperiores quas cupiditate dicta neque illo iusto. Voluptate laboriosam quod unde quae minima asperiores, sequi iste cupiditate sunt odio earum facere minus ad provident beatae esse animi consectetur dolorem eum nobis nisi eos vel pariatur sit? Asperiores omnis quos tempora tempore similique iste obcaecati sequi commodi dignissimos a provident laboriosam fuga, neque.</p>
+        <button>TEXT THIS</button>
+    </div>
+</section>
